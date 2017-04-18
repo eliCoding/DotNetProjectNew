041a714af3d5875f08c61bb5319232afa041eebb
mike <hartmut.yu@gmail.com>
2017-04-18 11:38:55 -0400

    addd array of product price

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,6 +24,17 @@ namespace PointOfSaleManagementSys
 
         Database db;
 
+        public int IdOfCategory;
+        public decimal[,] ProductPrice = { { 0.00m, 0.00m, 0.00m, 0.00m, 0.00m, 0.00m }, { 0.00m, 0.00m, 0.00m, 0.00m, 0.00m, 0.00m }, { 0.00m, 0.00m, 0.00m, 0.00m, 0.00m, 0.00m }, 
+                                         {0.00m, 0.00m, 0.00m, 0.00m,0.00m, 0.00m},{0.00m, 0.00m, 0.00m, 0.00m,0.00m, 0.00m},{0.00m, 0.00m, 0.00m, 0.00m,0.00m, 0.00m},};
+        string[,] items = new string[,]{
+                  {"Alexander keith ","Blond","BudWiser","Corona","lager","Staute"},
+                  {"Coffee","Green Tea","Tea", "Hot Chocolate","Cafe Latte","Cappuccino"}
+               ,{"Lasagna","Pasta","Pizza","Filet Mignon","Steak","Chicken Wing"},
+               {"Cheese Burger","General Tso","Koobide","Sushi","Ghormeh Sabzi","Poutine"},
+               {"Cheese Cake","Chocolate Cake","Tiramisu","Ice Cream Cake","Ginger Bread","Jelly"},
+               {"SYRAH","MERLOT","RIESLING","GEWÜRZTRAMINER","CHARDONNAY","PINOT NOIR"}
+        };
 
         double[,] itemCost = new double[,]{
                   /*{"Alexander keith ","Blond","BudWiser","Corona","lager","Staute"},
@@ -34,20 +45,12 @@ namespace PointOfSaleManagementSys
                {"SYRAH","MERLOT","RIESLING","GEWÜRZTRAMINER","CHARDONNAY","PINOT NOIR"}*/
         
         };
-        
+
         double iTax, iSubTotal, iTotal;
         List<Shopping> shoppingList = new List<Shopping>();
-        string[,] items = new string[,]{
-                  {"Alexander keith ","Blond","BudWiser","Corona","lager","Staute"},
-                  {"Coffee","Green Tea","Tea", "Hot Chocolate","Cafe Latte","Cappuccino"}
-               ,{"Lasagna","Pasta","Pizza","Filet Mignon","Steak","Chicken Wing"},
-               {"Cheese Burger","General Tso","Koobide","Sushi","Ghormeh Sabzi","Poutine"},
-               {"Cheese Cake","Chocolate Cake","Tiramisu","Ice Cream Cake","Ginger Bread","Jelly"},
-               {"SYRAH","MERLOT","RIESLING","GEWÜRZTRAMINER","CHARDONNAY","PINOT NOIR"}
-        
-        };
-      
-       
+
+
+
         public MainWindow()
         {
             try
@@ -62,17 +65,31 @@ namespace PointOfSaleManagementSys
             }
 
             InitializeComponent();
+            readProductPrice();
 
-          // LvItems.ItemsSource=db.GetAllCategory();
+
+
+            // LvItems.ItemsSource=db.GetAllCategory();
             //int CategoryId=1;
             //int ProductId=1;
-            LvShopping.Items.Add(db.GetProductbyID(1,1));
-            LvShopping.Items.Add(db.GetProductbyID(1, 2));
+
 
         }
 
         string currentItemText;
         int currentItemIndex;
+
+
+        private void readProductPrice()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    ProductPrice[i, j] = db.GetProductbyID(i, j).UnitPrice;
+                }
+            }
+        }
         private void ButtonBeer_Click(object sender, RoutedEventArgs e)
         {
             ItemList(0);
@@ -81,7 +98,7 @@ namespace PointOfSaleManagementSys
         {
             ItemList(1);
         }
-       
+
         private void ButtonDinner_Click(object sender, RoutedEventArgs e)
         {
             ItemList(2);
@@ -90,7 +107,7 @@ namespace PointOfSaleManagementSys
         {
             ItemList(3);
         }
-       
+
         private void ButtonDessert_Click(object sender, RoutedEventArgs e)
         {
             ItemList(4);
@@ -101,62 +118,62 @@ namespace PointOfSaleManagementSys
         }
         private void ItemList(int categoryId)
         {
-            int id = categoryId;
-           
             List<string> itemList = new List<string>();
-           
-            for (int i = 0; i < 6;  i++)
+
+            for (int i = 0; i < 6; i++)
             {
-                itemList.Add(items[id, i]);
-              
+                int j = i + 1 + (categoryId * 6);
+                Shopping p = db.GetProductbyID(categoryId + 1, j);
+                itemList.Add(p.ProductName + "  " + p.UnitPrice);
             }
             LvItems.ItemsSource = itemList;
+            IdOfCategory = categoryId;
         }
 
 
         private void ApplyDataBinding()
         {
-           
+
             List<string> itemList = new List<string>();
             // Bind ArrayList with the ListBox
-            LvItems.ItemsSource =itemList;
+            LvItems.ItemsSource = itemList;
         }
         private void ButtonAdd_Click(object sender, RoutedEventArgs e)
         {
-         
+
             currentItemText = LvItems.SelectedValue.ToString();
             currentItemIndex = LvItems.SelectedIndex;
-        
+
             Shopping s = new Shopping(currentItemText, 1, 3, 4, 3, 1);
-             LvShopping.Items.Add(s);
+            LvShopping.Items.Add(s);
 
-             int quantity = 0;
-             switch (currentItemText)
-             {
-                 case "Coffee":
-                     quantity++;
-                     break;
-                 case "Green Tea":
-                     break;
-                 case "Tea":
-                     break;
-                 case "Hot Chocolate":
-                     break;
-                 case "Cafe Latte":
-                     break;
-                 case "Cappuccino":
-                     break;
+            int quantity = 0;
+            switch (currentItemText)
+            {
+                case "Coffee":
+                    quantity++;
+                    break;
+                case "Green Tea":
+                    break;
+                case "Tea":
+                    break;
+                case "Hot Chocolate":
+                    break;
+                case "Cafe Latte":
+                    break;
+                case "Cappuccino":
+                    break;
 
 
-             }
-          
+            }
+
 
         }
 
 
         private void ButtonPrint_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
 
         private void ButtonDelete_Click(object sender, RoutedEventArgs e)
@@ -169,14 +186,13 @@ namespace PointOfSaleManagementSys
 
         }
 
-    
 
 
-     
 
-    
-     
 
-       
+
+
+
+
     }
 }
