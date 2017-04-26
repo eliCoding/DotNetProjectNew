6e8b7185242c5dc2b59376ade47d4e649e71f8d2
16FINAID-91798\Student <hartmut.yu@gmail.com>
2017-04-25 22:58:56 -0400

    console system updated

--- ManagementConsole/MainWindow.xaml.cs
+++ ManagementConsole/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,105 +22,108 @@ namespace ManagementConsole
     /// </summary>
     public partial class MainWindow : Window
     {
-        private void RefreshInStockList()
-        {
-            LvItems.ItemsSource = db.GetAllInStock();
-        }
-
-        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
-        {
-            int index = LvItems.SelectedIndex;
-            if (index < 0)
-            {
-                MessageBox.Show("Please select Item", "Warning", MessageBoxButton.OK, MessageBoxImage.Information);
-                return;
-            }
-            InStock i = (InStock)LvItems.Items[index];
-            try
-            {
-                db.DeleteInStockListById(i.Id);
-                RefreshInStockList();
-            }
-            catch (SqlException ex)
-            {
-                Console.WriteLine(ex.StackTrace);
-                MessageBox.Show("Database query error " + ex.Message);
-            }
-        }
-
-        private void ButtonEdit_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ButtonPrint_Click(object sender, RoutedEventArgs e)
-        {
-              
-        }
-
-        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
-        {
-            /* List<OrderList> oList = new List<OrderList>();
-                        int idx = LvItems.SelectedIndex;
-           
-                        if (idx < 0)
-                        {
-                            MessageBox.Show("Please select Item", "Warning", MessageBoxButton.OK, MessageBoxImage.Information);
-                            return;
-                        }
-                        decimal unitprice = ProductPrice[IdOfCategory, idx];
-                        string name = ProductName[IdOfCategory, idx];
-                        Counts[IdOfCategory, idx]++;
-                        int quantity = Counts[IdOfCategory, idx];
-                        int productId = idx + 1 + 6 * IdOfCategory;
-                        OrderList o = new OrderList(1, productId, quantity, unitprice, 0.1m);
-                        if (quantity ==1)
-                        {
-                            db.AddOrderList(o);
-                            RefreshShoppingList();
-                            return;
-                        }
-                        db.UpdateOrderList(o);
-                        RefreshShoppingList();*/
-=======
 
         private Database db1;
 
         public MainWindow()
         {
-<<<<<<< HEAD
-           db1 = new Database();
+            try
+            {
+                db1 = new Database();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.StackTrace);
+                MessageBox.Show("Error opening database connection: " + e.Message);
+                Environment.Exit(1);
+            }
             InitializeComponent();
-            ReadEmployee();
+            ReadAll();
+            TabControlConsole.SelectedIndex = 2;
+
         }
 
+        private void ReadAll()
+        {
+            ReadEmployee();
+            ReadOrders();
+            ReadStocks();
+        }
+        private void ReadOrders()
+        {
+            LvOrders.Items.Clear();
+            List<Order> orderAll = db1.GetAllOrders();
+            if (orderAll != null)
+            {
+                //  TabControlConsole.SelectedIndex = 1;
+                foreach (Order o in orderAll)
+                {
+                    LvOrders.Items.Add(o);
+                }
+            }
+        }
         private void ReadEmployee()
         {
-            //LvOrders.Items.Clear();
-            //List<Order> orderAll = db1.GetAllOrders();
-            //if (orderAll != null)
-            //{
-            //  //  TabControlConsole.SelectedIndex = 1;
-            //    foreach (Order o in orderAll)
-            //    {
-            //       LvOrders.Items.Add(o);
-            //    }
-            //}
-
             LvEmployee.Items.Clear();
             List<Employee> list = db1.GetAllEmployees();
             if (list != null)
             {
-             //   TabControlConsole.SelectedIndex = 2;
+                //   TabControlConsole.SelectedIndex = 2;
                 foreach (Employee ep in list)
                 {
                     LvEmployee.Items.Add(ep);
                 }
             }
-
         }
+        private void ReadStocks()
+        {
+            LvItems.Items.Clear();
+            List<InStock> itemStocks = db1.GetAllProducts();
+            if (itemStocks != null)
+            {
+                //   TabControlConsole.SelectedIndex = 2;
+                foreach (InStock ep in itemStocks)
+                {
+                    LvItems.Items.Add(ep);
+                }
+            }
+        }
+
         private void ButtonDelete_Click(object sender, RoutedEventArgs e)
         {
+            switch (TabControlConsole.SelectedIndex)
+            {
+                case 2:
+                    if (LvEmployee.SelectedIndex < 0)
+                    {
+                        MessageBox.Show("Please select a Order Item", "Warning", MessageBoxButton.OK, MessageBoxImage.Information);
+                        return;
+                    }
+                    Employee ep = (Employee)LvEmployee.Items[LvEmployee.SelectedIndex];
+                    db1.DeleteEmployeeById(ep.Id);
+                    ReadEmployee();
+                    break;
+                case 1:
+                    if (LvOrders.SelectedIndex < 0)
+                    {
+                        MessageBox.Show("Please select a Order Item", "Warning", MessageBoxButton.OK, MessageBoxImage.Information);
+                        return;
+                    }
+                    Order o = (Order)LvOrders.Items[LvOrders.SelectedIndex];
+                    db1.DeleteOrderById(o.OrderId);
+                    ReadOrders();
+                    break;
+                case 0:
+                    if (LvItems.SelectedIndex < 0)
+                    {
+                        MessageBox.Show("Please select a Order Item", "Warning", MessageBoxButton.OK, MessageBoxImage.Information);
+                        return;
+                    }
+                    InStock ins = (InStock)LvOrders.Items[LvOrders.SelectedIndex];
+                    db1.DeleteProductById(ins.Id);
+                    ReadStocks();
+                    break;
+            }
 
         }
 
