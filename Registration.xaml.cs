8c2cb3b5a66eb1b48623ff02a696e958c55f13db
16FINAID-91798\Student <hartmut.yu@gmail.com>
2017-04-30 18:18:30 -0400

    push login

--- Registration.xaml.cs
+++ Registration.xaml.cs
@@ -67,32 +67,62 @@ namespace PointOfSaleManagementSys
             {
                 string firstname = textBoxFirstName.Text;
                 string lastname = textBoxLastName.Text;
-                string email = textBoxEmail.Text;
+                string userName = textBoxEmail.Text;
                 string password = passwordBox1.Password;
 
-                if (passwordBox1.Password.Length < 6)
+                if (!ValidatePassword(password))
                 {
-                    Errormessage.Text = "Enter password more than 6 letters!";
+                    Errormessage.Text = "Password should be 8-15 letters, at least one low case letter, one upper case letter and one digit!";
                     passwordBox1.Focus();
                 }
-                if (passwordBox1.Password != passwordBoxConfirm.Password)
+                else if (passwordBox1.Password != passwordBoxConfirm.Password)
                 {
                     Errormessage.Text = "Confirm password must be same as password.";
                     passwordBoxConfirm.Focus();
                 }
 
-                if (db.ValidUserName(email))
+                else if (db.ValidUserName(userName))
                 {
                     Errormessage.Text = "The UseName has already been regiested!!!";
                     textBoxEmail.Focus();
                 }
                 else
                 {
-                    Employee ep = new Employee(0, firstname, lastname, email, password, 10000.0m);
+                    Employee ep = new Employee(0, firstname, lastname, userName, password, 10000.0m);
                     db.AddEmployee(ep);
                     Errormessage.Text = "*******User registered!******";
                 }
             }
+        }
+
+        static bool ValidatePassword( string password )
+        {
+            const int MIN_LENGTH =  8 ;
+            const int MAX_LENGTH = 15 ;
+
+            if ( password == null ) throw new ArgumentNullException() ;
+            bool meetsLengthRequirements = password.Length >= MIN_LENGTH && password.Length <= MAX_LENGTH ;
+            bool hasUpperCaseLetter      = false ;
+            bool hasLowerCaseLetter      = false ;
+            bool hasDecimalDigit         = false ;
+
+            if ( meetsLengthRequirements )
+            {
+                foreach (char c in password )
+                {
+                    if      ( char.IsUpper(c) ) hasUpperCaseLetter = true ;
+                    else if ( char.IsLower(c) ) hasLowerCaseLetter = true ;
+                    else if ( char.IsDigit(c) ) hasDecimalDigit    = true ;
+                }
+            }
+
+            bool isValid = meetsLengthRequirements
+                           && hasUpperCaseLetter
+                           && hasLowerCaseLetter
+                           && hasDecimalDigit
+                ;
+            return isValid ;
+
         }
     }
 }
